b7e05fc5bd14251189f35d0189e43f2b767688ed
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-07 14:42:00 +0100

    jawa

--- src/com/pingwit/part_3/homework_3/Task_1.java
+++ src/com/pingwit/part_3/homework_3/Task_1.java
@@ -7,14 +7,20 @@ public class Task_1 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the amount in dollars");
+        int amountInDollars = scanner.nextInt();
         int currencyld = scanner.nextInt();
-        double exchangeRateUSD = 0.25;
+        double exchangeRateUSD = 0.5;
 
         switch (currencyld) {
             case 1:
-                System.out.println("Amount in dollars" * exchangeRateUSD );
+                exchangeRateUSD = 0.5;
+
                 break;
+            default:
+                System.out.println("Your bank cannot exchenge currency");
         }
+        double convert = amountInDollars * exchangeRateUSD;
+        System.out.println("amount in dollars");
 
 
     }
