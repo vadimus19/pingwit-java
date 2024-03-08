05e0c8028a417739a57bf009fe61fa13672f8651
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-08 14:56:44 +0100

    jawa

--- src/com/pingwit/part_4/homework/Task_1.java
+++ src/com/pingwit/part_4/homework/Task_1.java
@@ -2,26 +2,23 @@ package com.pingwit.part_4.homework;
 
 public class Task_1 {
     public static void main(String[] args) {
-        double initialDistance = 10;
-        double growthRate = 10;
+        double distance = 10;
+        double procent = 10;
         int days = 7;
 
 
-        double totalDistance = calculateTotalDistance(initialDistance, growthRate, days);
-
-        System.out.printf("distance %.2f км  %d days .%n", totalDistance, days);
-    }
-
-    public static double calculateTotalDistance(double initialDistance, double growthRate, int days) {
         double totalDistance = 0;
-        double currentDistance = initialDistance;
+        double currentDistance = distance;
 
         for (int day = 1; day <= days; day++) {
             totalDistance += currentDistance;
-            currentDistance *= (1 + growthRate / 100);
+            currentDistance *= (1 + procent / 100);
         }
 
-        return totalDistance;
+
+        System.out.printf("Distance  %.2f км  %d Days.%n", totalDistance, days);
+
+
     }
 }
 
