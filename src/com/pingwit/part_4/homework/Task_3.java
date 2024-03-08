aa5397eb40b7ca4039b3416af19dd17dbddf5241
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-08 15:31:45 +0100

    jawa

--- src/com/pingwit/part_4/homework/Task_3.java
+++ src/com/pingwit/part_4/homework/Task_3.java
@@ -1,4 +1,14 @@
 package com.pingwit.part_4.homework;
 
 public class Task_3 {
+    public static void main(String[] args) {
+        int n = 256;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i *= 2) {
+            sum += i;
+        }
+
+        System.out.println("Result " + sum);
+    }
 }
