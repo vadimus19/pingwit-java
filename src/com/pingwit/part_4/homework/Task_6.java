20eece8aadc68d88bbc19190eeb900a368829b86
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-09 13:26:56 +0100

    jawa

--- src/com/pingwit/part_4/homework/Task_6.java
+++ src/com/pingwit/part_4/homework/Task_6.java
@@ -1,4 +1,12 @@
 package com.pingwit.part_4.homework;
 
 public class Task_6 {
-}
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i ++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+  }
