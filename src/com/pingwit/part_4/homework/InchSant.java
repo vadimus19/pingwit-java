421a2323ecb8b4f101b0b40916f7dab56ecf123c
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-12 22:46:45 +0100

    Update InchSant.java

--- src/com/pingwit/part_4/homework/InchSant.java
+++ src/com/pingwit/part_4/homework/InchSant.java
@@ -2,11 +2,11 @@ package com.pingwit.part_4.homework;
 
 public class InchSant {
     public static void main(String[] args) {
-
-
+//это лишняя строка, удали
+//это лишняя строка, удали
         for (int inches = 1; inches <= 20; inches++) {
             double centimeters = inches * 2.54;
-            System.out.println(inches + " " + centimeters);
+            System.out.println(inches + " " + centimeters); // попробуй вместо пустой строки добавить " | ", будет красивее
         }
     }
 }
