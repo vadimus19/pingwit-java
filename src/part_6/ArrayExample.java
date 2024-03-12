af913285a1571444254da47b7fdcf3973eba26c2
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-12 17:04:53 +0100

    jawa

---
+++ src/part_6/ArrayExample.java
@@ -0,0 +1,26 @@
+package part_6;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 100;
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("----");
+        Integer[] initArray = {9, 8, 7, 6, 5};
+        for (int i = 0; i < initArray.length; i++) {
+            System.out.println(initArray[i]);
+        }
+        System.out.println(Arrays.toString(initArray));
+        initArray = new Integer[]{0, 1};
+        initArray[0] = 33;
+        System.out.println(Arrays.toString(initArray));
+    }
+
+   }
+
