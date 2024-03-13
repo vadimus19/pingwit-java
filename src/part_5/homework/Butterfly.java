97bf7fc0dc8672495b512e35feae27fe1da3c673
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-03-13 12:43:23 +0100

    jawa

--- src/part_5/homework/Butterfly.java
+++ src/part_5/homework/Butterfly.java
@@ -2,6 +2,40 @@ package part_5.homework;
 
 public class Butterfly {
     public static void main(String[] args) {
+        int halfSize = 18 / 2;
 
+
+        for (int i = 0; i < halfSize; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 18 - 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i < 18; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+
+        for (int i = halfSize - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 18 - 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
