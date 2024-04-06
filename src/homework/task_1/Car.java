c691841265b88082c290a25fd10503e88a17b7a5
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-04-06 11:47:51 +0200

    homework 8 task_1

--- src/homework/task_1/Car.java
+++ src/homework/task_1/Car.java
@@ -5,50 +5,36 @@ public class Car {
     String marka = marka;
     int numDoors = numDoors;
 
-    public Car(String model, String marka, int numDoors) {
+    public String getModel() {
+        return model;
+    }
 
+    public void setModel(String model) {
         this.model = model;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
         this.marka = marka;
+    }
+
+    public int getNumDoors() {
+        return numDoors;
+    }
+
+    public void setNumDoors(int numDoors) {
         this.numDoors = numDoors;
-
     }
 
-    public Car() {
-        this("X5", "BMW", 4);
-        this("Mazda", "RX6", 2);
-        this("audi", "A8", 4);
-
-
-        public String getModel () {
-            return model;
-        }
-
-        public void setModel (String model){
-            this.model = model;
-        }
-
-        public String getMarka () {
-            return marka;
-        }
-
-        public void setMarka (String marka){
-            this.marka = marka;
-        }
-
-        public int getNumDoors () {
-            return numDoors;
-        }
-
-        public void setNumDoors ( int numDoors){
-            this.numDoors = numDoors;
-        }
-
-        @Override
-        public String toString () {
-            return "Car{" +
-                    "model='" + model + '\'' +
-                    ", marka='" + marka + '\'' +
-                    ", numDoors=" + numDoors +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", marka='" + marka + '\'' +
+                ", numDoors=" + numDoors +
+                '}';
     }
+}
--- src/homework/task_1/CarMarka.java
+++ src/homework/task_1/CarMarka.java
@@ -2,11 +2,35 @@ package homework.task_1;
 
 public class CarMarka {
     public void main(String[] args, String CarMarka) {
-       for Car Car = new Car();
-       if (CarMarka == "audi");
-       System.out.println("audi" + "Door num 2 is open");
+
+        Car car1 = new Car();
+        car1.setModel("X3");
+        car1.setMarka("BMW");
+        car1.setNumDoors(4);
 
 
+        Car car2 = new Car();
+        car2.setModel("RX5");
+        car2.setMarka("Mazda");
+        car2.setNumDoors(2);
 
+        Car car3 = new Car();
+        car3.setModel("A8");
+        car3.setMarka("Audi");
+        car3.setNumDoors(4);
+
+        Car[] cars = {car1, car2, car3};
+
+        for (Car car : cars) {
+            if (car.getMarka() == ("Audi")) {
+                System.out.println("Opening doors for Audi " + car.getModel() + ":");
+
+            }
+        }
     }
 }
+
+
+
+
+
