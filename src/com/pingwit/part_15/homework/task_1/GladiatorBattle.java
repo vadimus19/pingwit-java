1757c7efe1fa9c156d7371e6dd2b761a4065c02d
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-06-05 13:39:17 +0200

    part_15-16

--- src/com/pingwit/part_15/homework/task_1/GladiatorBattle.java
+++ src/com/pingwit/part_15/homework/task_1/GladiatorBattle.java
@@ -4,79 +4,77 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GladiatorBattle {
+    private static final int MAX_MISSES = 4;
+    private static final int ACTIONS = 3;
+    private static final int BLOCKS = 3;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
         String playerName = scanner.nextLine();
 
-        String[] opponents = {"Maximus", "Achilles", "Spartacus", "Vadimus"};
+        String[] opponents = {"Maximus", "Achilles", "Spartacus", "Vadimus Velikus"};
         String opponentName = opponents[(int) (Math.random() * opponents.length)];
 
-        System.out.println("Fight " + playerName + " vs " + opponentName);
-        System.out.println("Are you READYYYYYY? (Press enter )");
+
+        System.out.println("Battle " + playerName + " vs " + opponentName);
+        System.out.println("Are you READYYYYY? (Press Enter)");
         scanner.nextLine();
 
         int playerMissCount = 0;
         int opponentMissCount = 0;
-        int bit = 4;
-        int block = 4;
-        int bound = 4;
         Random random = new Random();
 
-        while (playerMissCount < 4 && opponentMissCount < 4) {//magic number - магическое число еще не поправил
-
-            int playerAttack; // ты не используешь переменную playerAttack за пределами блока try, поэтому не имеет смысла ее здесь создавать
+        while (playerMissCount < MAX_MISSES && opponentMissCount < MAX_MISSES) {
             try {
-                System.out.println("Select an action:");
-                System.out.println("1 - hit in the head");
-                System.out.println("2 - hit to the body");
-                System.out.println("3 - hit in the legs");
-                playerAttack = scanner.nextInt(); //  playerAttack = scanner.nextInt(); -> int playerAttack = scanner.nextInt();
+                System.out.println("Choose an action:");
+                System.out.println("1 - head strike");
+                System.out.println("2 - body strike");
+                System.out.println("3 - leg strike");
+                int playerAttack = scanner.nextInt();
 
-                if (playerAttack < 1 || playerAttack > bit) {// magic number
-                    throw new BattleException("Wrong action. Try again.");
+                if (playerAttack < 1 || playerAttack > ACTIONS) {
+                    throw new BattleException("Invalid action. Try again.");
                 }
 
-                int opponentBlock = random.nextInt(bound);
-
-                int opponentAttack = random.nextInt(bound);
+                int opponentBlock = random.nextInt(BLOCKS);
                 if (playerAttack != opponentBlock) {
                     opponentMissCount++;
-                    System.out.println("Miss! " + opponentName + " attacking " + opponentAttack); // не очень понял почему этот вывод на консоль ты решил здесь сделать. Получается я заранее вижу как будет атаковать соперник и могу выбрать правильный блок
+                    System.out.println("Miss! " + opponentName + " attacks!");
                 } else {
-                    System.out.println("block! " + opponentName + " try to attacked, you made a block!");
+                    System.out.println("Block! " + opponentName + " tried to attack, but you blocked it!");
                 }
 
-                System.out.println("Select an action:");
+                int opponentAttack = random.nextInt(BLOCKS);
+                System.out.println("Choose an action:");
                 System.out.println("1 - upper block");
                 System.out.println("2 - middle block");
                 System.out.println("3 - lower block");
                 int playerBlock = scanner.nextInt();
 
-                if (playerBlock < 1 || playerBlock > block) {
-                    throw new BattleException("Wrong action. Try again.");
+                if (playerBlock < 1 || playerBlock > BLOCKS) {
+                    throw new BattleException("Invalid action. Try again.");
                 }
 
-                playerAttack = random.nextInt(bound);
                 if (opponentAttack != playerBlock) {
                     playerMissCount++;
-                    System.out.println("Miss! " + playerName + " attack  " + playerAttack);
+                    System.out.println("Miss! " + opponentName + " attacked!");
                 } else {
-                    System.out.println("Block! " + playerName + " try to attacked, but " + opponentName + " blocked!");
+                    System.out.println("Block! " + opponentName + " tried to attack, but you blocked it!");
                 }
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
+                scanner.nextLine();
             }
         }
 
-        if (playerMissCount == 4) {
-            System.out.println("You loose! " + opponentName + " Win!");
+        if (playerMissCount == MAX_MISSES) {
+            System.out.println("You lost! " + opponentName + " won!");
         } else {
-            System.out.println("YOU WIN " + opponentName + "!");
+            System.out.println("YOU WON! " + opponentName + " lost!");
         }
 
     }
 }
-
 
