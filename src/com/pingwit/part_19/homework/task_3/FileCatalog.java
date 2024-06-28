0365c8d463cbc20d94f15b4b52eebfa39fc4d8b8
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-06-28 12:18:32 +0200

    homework 19

--- src/com/pingwit/part_19/homework/task_3/FileCatalog.java
+++ src/com/pingwit/part_19/homework/task_3/FileCatalog.java
@@ -1,14 +1,8 @@
 package com.pingwit.part_19.homework.task_3;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
-public class ZipInputStreamHomework {
+public class FileCatalog {
     public static void main(String[] args) throws IOException {
         File file = new File("src/com/pingwit/part_19/homework");
         System.out.println(file.isFile());
@@ -18,4 +12,4 @@ public class ZipInputStreamHomework {
 
 
     }
-    }
+}
