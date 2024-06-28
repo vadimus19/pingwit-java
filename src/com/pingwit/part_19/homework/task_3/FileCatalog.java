3a8b46a4da46b49dda5aeb9aa2a43136b198f925
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-06-28 13:02:22 +0200

    homework 19.

--- src/com/pingwit/part_19/homework/task_3/FileCatalog.java
+++ src/com/pingwit/part_19/homework/task_3/FileCatalog.java
@@ -1,15 +1,21 @@
 package com.pingwit.part_19.homework.task_3;
 
 import java.io.*;
+import java.nio.file.Files;
 
 public class FileCatalog {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/com/pingwit/part_19/homework");
+        File file = new File("src/com/pingwit/part_19");
         System.out.println(file.isFile());
         System.out.println(file.isDirectory());
-        System.out.println(file.listFiles());
-        System.out.println(file.getAbsoluteFile());
+        System.out.println(file.listFiles().toString());
+        System.out.println(file.getAbsoluteFile().getName());
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                System.out.println(f.toString());
 
-
+            }
+        }
     }
 }
