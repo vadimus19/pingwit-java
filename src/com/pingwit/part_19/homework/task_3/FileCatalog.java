82c0d56cd11633465788f4268f10d2a445699834
vadimdanilchenko <vadimdanilchenko1991@gmail.com>
2024-07-05 11:58:40 +0200

    homework 21.

--- src/com/pingwit/part_19/homework/task_3/FileCatalog.java
+++ src/com/pingwit/part_19/homework/task_3/FileCatalog.java
@@ -1,41 +1,26 @@
 package com.pingwit.part_19.homework.task_3;
 
 import java.io.*;
-import java.nio.file.Files;
 
 public class FileCatalog {
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/alexfomin/IdeaProjects/pingwit/pingwit-java-students/pingwit-java/src/com/pingwit/part_19");
-        System.out.println(file.isFile());
-        System.out.println(file.isDirectory());
-        /*
-        1. file.listFiles().toString() - toString() будет вызван автоматически, поэтому  System.out.println(file.listFiles())
-        2. System.out.println(file.listFiles()) - если просто передать массив в System.out.println(), то будет распечатана ссылка, а не содержимое массива
-         */
-        System.out.println(file.listFiles().toString());
-        System.out.println(file.getAbsoluteFile().getName());
-        File[] files = file.listFiles();
-        if (files != null) {
-            for (File f : files) {
-                System.out.println(f.toString());
+        File file = new File("src/com/pingwit/part_19");
+        printFileStructure("", file.listFiles());
+    }
+
+    private static void printFileStructure(String tab, File[] files) {
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            } else {
+                System.out.println(file.getName());
+
+                printFileStructure("", file.listFiles());
 
             }
+
         }
-
-        /*
-        Для решения это задачи понадобится рекурсия.
-        Псевдокод:
-        если файл -> печатаем названия файла
-        иначе -> добавляем отступ и рекурсивно вызываем этот же метод
-
-        Давай не просто печатать весь путь до файла, а красивую структуру, пример:
-        part_19
-            FilesExample.java
-            Work.java
-            homework
-                task_1
-                task_2
-                ...
-         */
     }
 }
+
+
--- src/com/pingwit/part_20/homework/task_1/Garage.java
+++ src/com/pingwit/part_20/homework/task_1/Garage.java
@@ -6,15 +6,19 @@ import java.util.List;
 // отформатируй код
 public class Garage<t extends Car> { // t -> T
     private List<t> cars;
+
     public Garage() {
         cars = new ArrayList<>();
     }
+
     public void park(t car) {
         cars.add(car);
     }
+
     public List<t> getCars() {
         return cars;
     }
+
     public long countBlackCars() {
         return cars.stream().filter(car -> "Black".equalsIgnoreCase(car.getColor())).count(); // обычно  после .stream() каждый следующий метод переносят на новую строку, так удобнее читать код
     }
